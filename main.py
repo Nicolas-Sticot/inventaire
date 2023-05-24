e40ebe291028b31682d767bde6fa9fffb204e41f
Nicolas-Sticot <nlv022000@gmail.com>
2023-05-24 21:49:25 +0200

    Import test

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 import tkinter as tk
+from tkinter import filedialog
 import numpy as np
 import pandas as pd
 
+
+class InventorySelect(tk.Frame):
+
+    def __init__(self, root):
+        super().__init__(root)
+        self.inv_file = None
+        self._change_inv_file()
+        self._create_gui()
+        self.pack()
+
+    def _create_gui(self):
+        box_inv_file = tk.Label(self, text=self.inv_file)
+        box_inv_file.pack()
+
+    def _change_inv_file(self):
+        self.inv_file = filedialog.askopenfilename()
+        print(self.inv_file)
+
+
 def main():
-  print("Test")
+    app = tk.Tk()
+    app.title("Gestionnaire d'inventaire")
+    InventorySelect(app)
+    app.mainloop()
+
+
+if __name__ == "__main__":
+    main()
+
